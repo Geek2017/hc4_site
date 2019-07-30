119760f9fe01b446eb7b9e1c27ede99715fe7bfd
jakembalbedina <jakembalbedina@gmail.com>
2019-07-30 17:31:55 +0800

    try phpmailer and fixed designs

--- mailer_try.php
+++ mailer_try.php
@@ -1,13 +1,13 @@
 <?php
 
-//use PHPMailer\PHPMailer\PHPMailer;
-//use PHPMailer\PHPMailer\Exception;
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\Exception;
 
-require '/PHPMailer/src/Exception.php';
-require '/PHPMailer/src/PHPMailer.php';
-require '/PHPMailer/src/SMTP.php';   
+require './PHPMailer/src/Exception.php';
+require './PHPMailer/src/PHPMailer.php';
+require './PHPMailer/src/SMTP.php';   
 
-    if( !empty($_POST["name"]) && !empty($_POST["email"]) && !empty($_POST["message"])){ 
+    if( !empty($_POST["name"]) && !empty($_POST["email"]) && !empty($_POST["message"])){
 
         $name = strip_tags(trim($_POST["name"]));
         $email = filter_var(trim($_POST["email"]), FILTER_SANITIZE_EMAIL);
@@ -17,21 +17,21 @@ require '/PHPMailer/src/SMTP.php';
         $mail->IsSMTP();           
 
         //Server settings
-        $mail->SMTPDebug = 2;                                       // Enable verbose debug output
-        $mail->isSMTP();                                            // Set mailer to use SMTP
-        $mail->Host       = 'smtp.gmail.com';                       // Specify main and backup SMTP servers
-        $mail->SMTPAuth   = true;                                   // Enable SMTP authentication
-        $mail->Username   = 'jakembalbedina@gmail.com';                     // SMTP username
+        $mail->SMTPDebug = 2;                                           // Enable verbose debug output
+        $mail->isSMTP();                                                // Set mailer to use SMTP
+        $mail->Host       = 'smtp.gmail.com';                           // Specify main and backup SMTP servers
+        $mail->SMTPAuth   = true;                                       // Enable SMTP authentication
+        $mail->Username   = 'jakembalbedina@gmail.com';                 // SMTP username
         $mail->Password   = 'Secret123;';                               // SMTP password
-        $mail->SMTPSecure = 'tls';                                  // Enable TLS encryption, `ssl` also accepted
-        $mail->Port       = 587;                                    // TCP port to connect to
+        $mail->SMTPSecure = 'tls';                                      // Enable TLS encryption, `ssl` also accepted
+        $mail->Port       = 587;                                        // TCP port to connect to
 
         //Recipients
-        $mail->setFrom($email);
-        //$mail->addAddress($email, $name);                           // Add a recipient
+        $mail->setFrom('jakembalbedina@gmail.com', 'PacificBlueIT');    // FROM:
+        $mail->addAddress($email, $name);                               // TO: Add a recipient
 
         // Content
-        $mail->isHTML(true);                                  // Set email format to HTML
+        $mail->isHTML(true);                                        // Set email format to HTML
         $mail->Subject = "New contact from $name";
         $mail->Body    = $message ;
         //$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
@@ -41,5 +41,12 @@ require '/PHPMailer/src/SMTP.php';
         } else {
             echo "Message has been sent";
         }
+    }
+
+    else if ( empty($name) || empty($message) || !filter_var($email, FILTER_VALIDATE_EMAIL)) {
+        // Set a 400 (bad request) response code and exit.
+        http_response_code(400);
+        echo "Oops! There was a problem with your submission. Please complete the form and try again.";
+        exit;
     }
  ?>
